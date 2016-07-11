509beaba5caa92235e21bda4fc43a3e19c53b1d1
Stephen Hosom <0xhosom@gmail.com>
2016-07-11 10:47:00 -0400

    additional checks

--- brando/brando.py
+++ brando/brando.py
@@ -14,6 +14,8 @@ _HEADER = '\t'.join([
 		'meta.do_notice'
 	])
 
+_HASH_PATTERN = re.compile('([a-fA-F0-9]{64}|[a-fA-F0-9]{40}|[a-fA-F0-9]{32})')
+
 class IndicatorType(Enum):
 	ADDR = 'ADDR'
 	URL = 'URL'
@@ -59,6 +61,14 @@ class Document:
 	@indicator.setter
 	def indicator(self, indicator):
 
+		if type(indicator) is not str:
+			raise AttributeError('indicator must be a string')
+
+		# Validate for empty strings
+		indicator = indicator.strip()
+		if not indicator:
+			raise AttributeError('indicator connot be empty')
+
 		# Validate IP Address indicator types
 		if self.indicator_type == IndicatorType.ADDR:
 			ip = ip_address(indicator)
@@ -68,6 +78,10 @@ class Document:
 			url = urlparse(indicator)
 			indicator = url.geturl().replace('{0}://'.format(url.scheme), '')
 
+		if (self.indicator_type == IndicatorType.FILE_HASH):
+			if not _HASH_PATTERN.match(indicator):
+				raise AttributeError('indicator does not appear to be a valid hash')
+				
 		self._indicator = indicator
 
 	@property
@@ -125,9 +139,22 @@ class Document:
 		return '\t'.join(fields)
 
 def bro_print(documents, fpath=None):
+	'''Print a list of deduplicated documents as a Bro Intel file. 
 
+	Note: Any differences in documents will result in them not being
+	deduplcated. If deduplication is desired, then all sub fields must
+	be the same.
+
+	Args:
+		documents (list of Document): The list of documents to be printed as 
+		an intel file for Bro to consume.
+		fpath (str): The filepath to write the Bro file to. If none is
+		passed, then the intel file will be printed to stdout.
+	Returns:
+		None
+	'''
 	documents = set(documents)
 	print(_HEADER, file=None)
 	for document in documents:
 		if type(document) == Document:
-			print(document)
+			print(document, file=None)
