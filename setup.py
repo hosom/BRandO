5863f16a956163e267d1912b5b4a4c491b89a84a
Stephen Hosom <0xhosom@gmail.com>
2017-01-24 14:38:29 +0000

    updates to namespace

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='brando',
+setup(name='brawndo',
 	version='0.1',
 	description='Python module for creating Bro intel files.',
 	author='Stephen Hosom',
 	author_email='0xhosom@gmail.com',
-	url='https://github.com/hosom/brando',
-	packages=['brando'],
+	url='https://github.com/hosom/brawndo',
+	packages=['brawndo'],
 	install_requires=[],
 	)
